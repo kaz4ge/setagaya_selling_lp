68f49f574f52898fea429c9189b3e9ebe1047039
山口 一重 <yamaguchi-kazushige@ad2013.thr.gr.jp>
2025-12-06 19:21:53 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// assets/js/script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const yearEl = document.getElementById("year");
+  if (yearEl) yearEl.textContent = new Date().getFullYear();
+});
